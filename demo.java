df7f80a356f8496e4412ba130b04f8e152cc3e15
Chethan <chethankumar9886@gmail.com>
2026-05-04 21:43:13 +0530

    added demo.java

---
+++ demo.java
@@ -0,0 +1,7 @@
+// demo.java is added to the project. This file contains a simple Java program that demonstrates basic syntax and functionality of the Java programming language. The program includes a main method that prints "Hello, World!" to the console, as well as some comments explaining the code. This file serves as a starting point for learning Java and can be expanded upon with additional features and functionality as needed.```java
+public class demo {
+    public static void main(String[] args) {
+        // This is a simple Java program that prints "Hello, World!" to the console.
+        System.out.println("Hello, World!");
+    }
+}
